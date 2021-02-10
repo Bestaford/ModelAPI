df95bea2f35f0233f996ee2a98f04e99e0d3a3f7
Bestaford <butenko1demitr@gmail.com>
2021-02-10 14:18:55 +0300

    Initial commit

---
+++ src/main/java/com/bestaford/Model.java
@@ -0,0 +1,12 @@
+package com.bestaford;
+
+import cn.nukkit.entity.EntityHuman;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+
+public class Model extends EntityHuman {
+
+    public Model(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+}
